03fe76e82affe6d17cfc1ce05b930ebeee96041c
Umar Ishtiaq <umar93922@gmail.com>
2022-05-01 16:59:19 +0500

    initial js

---
+++ 2-clock/js/main.js
@@ -0,0 +1,9 @@
+const secondHand = document.querySelector('.second-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand= document.querySelector('.hour-hand');
+
+function setDate(){
+    let now = new Date();
+    console.log(now)
+}
+setDate()
