666eda04c53e5d66ff4c238c19613261e0f85f61
Umar Ishtiaq <umar93922@gmail.com>
2022-05-02 01:55:31 +0500

    Clock Done

--- 2-clock/js/main.js
+++ 2-clock/js/main.js
@@ -3,7 +3,23 @@ const minHand = document.querySelector('.min-hand');
 const hourHand= document.querySelector('.hour-hand');
 
 function setDate(){
-    let now = new Date();
-    console.log(now)
+    const now = new Date();
+    
+    const seconds = now.getSeconds();
+    const secondDeg = ((seconds/60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondDeg}deg)`
+
+
+    const minutes = now.getMinutes();
+    const minDeg = ((minutes/60) * 360) + 90;
+    minHand.style.transform = `rotate(${minDeg}deg)`
+
+
+    const hours = now.getHours();
+    const hourDeg = ((hours/12) * 360) + 90;
+    hourHand.style.transform = `rotate(${hourDeg}deg)`
 }
+
+setInterval(setDate, 1000)
+
 setDate()
