ec45e890c4ae07a9f59429fe793257780250c354
Umar Ishtiaq <umar93922@gmail.com>
2022-05-02 16:11:41 +0500

    Array Cardio Started

---
+++ 4-ArrayCardioDay1/js/main.js
@@ -0,0 +1,27 @@
+const inventors = [
+    { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
+    { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
+    { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
+    { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
+    { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
+    { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
+    { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
+    { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 },
+    { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 },
+    { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
+    { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
+    { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 }
+  ];
+//FILTER
+//   const list = inventors.filter(obj => {
+//       return obj.year > 1500 && obj.year < 1600;
+//   })
+//   console.log(list)
+
+
+  //MAP
+  const names = inventors.map(obj => obj.first + ' ' + obj.last)
+  console.log(names)
+
+  const years = inventors.reduce((acc, c) => acc + (c.passed - c.year), 0)
+  console.log(years)
